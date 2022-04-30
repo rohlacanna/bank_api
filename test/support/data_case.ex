92969c42f024a603c5b5fa6c8d94630cc01638b1
Rômulo Silva <romulo_lss@hotmail.com>
2022-04-30 18:52:01 -0300

    feat: Config credo

--- test/support/data_case.ex
+++ test/support/data_case.ex
@@ -44,7 +44,7 @@ defmodule BankApi.DataCase do
 
   """
   def errors_on(changeset) do
-    Changeset.traverse_errors(changeset, fn {message, opts} ->
+    Ecto.Changeset.traverse_errors(changeset, fn {message, opts} ->
       Regex.replace(~r"%{(\w+)}", message, fn _, key ->
         opts |> Keyword.get(String.to_existing_atom(key), key) |> to_string()
       end)
