2cb1eae496ee5a9168002d6d5df6b8cd6c444b2e
Rômulo Silva <romulo_lss@hotmail.com>
2022-05-22 20:36:11 -0300

    Implement Banking.Account creation

---
+++ test/bank_api/banking_test.exs
@@ -0,0 +1,35 @@
+defmodule BankApi.BankingTest do
+  use BankApi.DataCase
+
+  import BankApi.Factory
+
+  alias BankApi.Banking
+  alias BankApi.Banking.Account
+  alias BankApi.Users.User
+
+  describe "account" do
+    test "list_account/0 returns all account" do
+      account = insert(:account)
+      assert Banking.list_account() == [account]
+    end
+
+    test "get_account!/1 returns the account with given id" do
+      account = insert(:account)
+      assert Banking.get_account!(account.id) == account
+    end
+
+    test "create_account/1 with valid data creates a account" do
+      user = insert(:user)
+
+      assert {:ok, %Account{} = account} = Banking.create_account(user)
+      assert account.current_balance == Decimal.new("0.0")
+      assert account.user_id == user.id
+    end
+
+    test "create_account/1 with invalid data returns error changeset" do
+      fake_user = %User{id: Ecto.UUID.generate()}
+      assert {:error, %Ecto.Changeset{} = changeset} = Banking.create_account(fake_user)
+      assert errors_on(changeset) == %{user: ["does not exist"]}
+    end
+  end
+end
